5eea28b95080c9ed7e393334dec6ce1ac571bda6
jmhatch <jmhatch@vt.edu>
2021-02-10 16:27:49 -0500

    Update repo and workflow

--- scripts/plot_covid_data_from_nyt.R
+++ scripts/plot_covid_data_from_nyt.R
@@ -1,12 +1,16 @@
+# load libraries
+library(dplyr)
+library(ggplot2)
+
 # load NYT data
-ma_week = read_csv('./data/ma-counties.csv')
+ma_week = readr::read_csv('./data/ma-counties.csv')
 
 # plot
 covid_plt = ggplot(data = ma_week, aes(x = cases, y = week_sum, group = county)) + 
   geom_line() + 
-  geom_point(data = ma_week %>% filter(date == max(date)), size = 2, color = 'red') +
+  geom_point(data = ma_week %>% dplyr::filter(date == max(date)), size = 2, color = 'red') +
   facet_wrap(~county) + 
-  gghighlight(county == county, use_direct_label = FALSE) +
+  gghighlight::gghighlight(county == county, use_direct_label = FALSE) +
   scale_x_log10(labels = scales::trans_format('log10', scales::math_format(10^.x))) +
   scale_y_log10(labels = scales::trans_format('log10', scales::math_format(10^.x))) +
   theme_bw() +
--- scripts/process_covid_data_from_nyt.R
+++ scripts/process_covid_data_from_nyt.R
@@ -1,23 +1,27 @@
+# load libraries
+library(dplyr)
+library(magrittr)
+
 # load NYT data
-nyt_data = read_csv('./data/us-counties.csv')
+nyt_data = readr::read_csv('./data/us-counties.csv')
 
 # filter to get Massachusetts data
-ma_nyt = nyt_data %>% filter(state == 'Massachusetts')
+ma_nyt = nyt_data %>% dplyr::filter(state == 'Massachusetts')
 
 # reverse the cumulative sum of covid-19 counts and name that column new_cases
-ma_nyt %<>% group_by(county) %>% arrange(date) %>% mutate(new_cases = c(cases[1], diff(cases))) %>% ungroup()
+ma_nyt %<>% dplyr::group_by(county) %>% dplyr::arrange(date) %>% dplyr::mutate(new_cases = c(cases[1], diff(cases))) %>% dplyr::ungroup()
 
 # take a weekly rolling sum of new_cases
-ma_week = ma_nyt %>% group_by(county) %>% arrange(date) %>% mutate(week_sum = roll_sum(new_cases, 7, align = 'right', fill = NA)) %>% ungroup()
+ma_week = ma_nyt %>% dplyr::group_by(county) %>% dplyr::arrange(date) %>% dplyr::mutate(week_sum = RcppRoll::roll_sum(new_cases, 7, align = 'right', fill = NA)) %>% dplyr::ungroup()
 
 # set minimum number of total confirmed cases to 10
-ma_week %<>% filter(cases >= 10 & !is.na(week_sum))
+ma_week %<>% dplyr::filter(cases >= 10 & !is.na(week_sum))
 
 # remove data where county is unknown or from Martha's Vineyard or Nantucket
-ma_week %<>% filter(!county %in% c('Unknown', 'Dukes', 'Nantucket'))
+ma_week %<>% dplyr::filter(!county %in% c('Unknown', 'Dukes', 'Nantucket'))
 
 # handle 0 new cases
-ma_week %<>% filter(week_sum > 0)
+ma_week %<>% dplyr::filter(week_sum > 0)
 
 ## save data
 write.csv(ma_week, './data/ma-counties.csv', row.names = FALSE)
