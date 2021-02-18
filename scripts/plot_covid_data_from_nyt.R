9f7a0814d868c19603269c24826c9de9e05666b3
jmhatch <jmhatch@vt.edu>
2021-02-18 16:56:30 -0500

    Added some content to focus on Bristol County

--- scripts/plot_covid_data_from_nyt.R
+++ scripts/plot_covid_data_from_nyt.R
@@ -21,3 +21,21 @@ covid_plt = ggplot(data = ma_week, aes(x = cases, y = week_sum, group = county))
 png(file = './images/ma_county_covid.png',  width = 735, height = 526)
   print(covid_plt)
 dev.off()
+
+# Bristol county in particular
+bristol_plt = ggplot(data = ma_week %>% dplyr::filter(county == 'Bristol'), aes(x = cases, y = week_sum, group = county)) + 
+  geom_line() + 
+  geom_point(data = ma_week %>% dplyr::filter(date == max(date) & county == 'Bristol'), size = 2, color = 'red') +
+  facet_wrap(~county) + 
+  gghighlight::gghighlight(county == county, use_direct_label = FALSE) +
+  scale_x_log10(labels = scales::trans_format('log10', scales::math_format(10^.x))) +
+  scale_y_log10(labels = scales::trans_format('log10', scales::math_format(10^.x))) +
+  theme_bw() +
+  xlab('total confirmed cases') + 
+  ylab('new confirmed cases (in the past week)') + 
+  geom_text(data = ma_week %>% dplyr::filter(date == max(date) & county == 'Bristol'), aes(label = date), hjust = 1, vjust = 1.5) 
+
+# save plot
+png(file = './images/bristol_county_covid.png',  width = 735, height = 526)
+  print(bristol_plt)
+dev.off()
