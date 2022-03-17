c4d22f01c73808ab0d942c37e99bd184c8364e7c
jmhatch <jmhatch@vt.edu>
2022-03-17 14:59:23 -0400

    Test GH action

--- scripts/process_covid_data_from_cdc.R
+++ scripts/process_covid_data_from_cdc.R
@@ -15,14 +15,17 @@ cdc_comm_ma %<>% dplyr::filter(!county %in% c('Dukes County, MA', 'Nantucket Cou
 # download CDC transmission levels
 cdc_tran_data = 'https://data.cdc.gov/api/views/8396-v7yb/rows.csv?accessType=DOWNLOAD' %>% url() %>% readr::read_csv()
 
-# filter to get Massachusetts data
-cdc_tran_ma = cdc_tran_data %>% filter(state_name == 'Massachusetts' & report_date == max(report_date))
-
-# remove data where county is Martha's Vineyard or Nantucket
-cdc_tran_data %<>% dplyr::filter(!county_name %in% c('Dukes County', 'Nantucket County'))
-
-# combine county-level data
-cdc_county = cdc_comm_ma %>% dplyr::select(state, state_abb, county, date_updated, `covid-19_community_level`) %>% rename(date_community = date_updated) %>% left_join(cdc_tran_ma %>% dplyr::select(state_name, county_name, report_date, community_transmission_level) %>% rename(state = state_name, county = county_name, date_transmission = report_date), by = c('state', 'county'))
+# # filter to get Massachusetts data
+# cdc_tran_ma = cdc_tran_data %>% filter(state_name == 'Massachusetts' & report_date == max(report_date))
+# 
+# # remove data where county is Martha's Vineyard or Nantucket
+# cdc_tran_data %<>% dplyr::filter(!county_name %in% c('Dukes County', 'Nantucket County'))
+# 
+# # combine county-level data
+# cdc_county = cdc_comm_ma %>% 
+#   dplyr::select(state, state_abb, county, date_updated, `covid-19_community_level`) %>% 
+#   rename(date_community = date_updated) %>% 
+#   left_join(cdc_tran_ma %>% dplyr::select(state_name, county_name, report_date, community_transmission_level) %>% rename(state = state_name, county = county_name, date_transmission = report_date), by = c('state', 'county'))
 
 # save data
-write.csv(cdc_county, './data/cdc-ma-counties.csv', row.names = FALSE)
+write.csv(cdc_tran_data, './data/cdc-ma-counties.csv', row.names = FALSE)
