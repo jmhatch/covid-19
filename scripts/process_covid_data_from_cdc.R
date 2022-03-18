99af75008259433d66b516db10ebe532e4ddfe48
jmhatch <jmhatch@vt.edu>
2022-03-18 08:17:08 -0400

    Test GH action

--- scripts/process_covid_data_from_cdc.R
+++ scripts/process_covid_data_from_cdc.R
@@ -13,13 +13,13 @@ cdc_comm_ma = cdc_comm_data %>% dplyr::filter(state == 'Massachusetts' & date_up
 cdc_comm_ma %<>% dplyr::filter(!county %in% c('Dukes County, MA', 'Nantucket County, MA')) %>% tidyr::separate(county, c('county', 'state_abb'), sep = ',')
 
 # download CDC transmission levels
-cdc_tran_data = 'https://data.cdc.gov/api/views/8396-v7yb/rows.csv?accessType=DOWNLOAD' %>% url() %>% readr::read_csv()
+cdc_tran_data = 'https://data.cdc.gov/api/views/8396-v7yb/rows.csv?accessType=DOWNLOAD' %>% url() %>% read.csv(., header = TRUE)
 
 # filter to get Massachusetts data
-cdc_tran_ma = cdc_tran_data %>% dplyr::filter(state_name == 'Massachusetts' & report_date == max(report_date))
-# 
+cdc_tran_ma = cdc_tran_data %>% dplyr::mutate(report_date = as.Date(report_date, format = '%Y/%m/%d')) %>% dplyr::filter(state_name == 'Massachusetts' & report_date == max(report_date))
+
 # # remove data where county is Martha's Vineyard or Nantucket
-# cdc_tran_ma %<>% dplyr::filter(!county_name %in% c('Dukes County', 'Nantucket County'))
+cdc_tran_ma %<>% dplyr::filter(!county_name %in% c('Dukes County', 'Nantucket County'))
 
 # # combine county-level data
 # cdc_county = cdc_comm_ma %>%
