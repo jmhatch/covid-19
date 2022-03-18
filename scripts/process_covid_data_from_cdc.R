113b55c931a080d012e55977e75e4b66bc88226d
jmhatch <jmhatch@vt.edu>
2022-03-18 08:48:26 -0400

    Test GH action

--- scripts/process_covid_data_from_cdc.R
+++ scripts/process_covid_data_from_cdc.R
@@ -4,10 +4,10 @@ library(magrittr)
 library(tidyr)
 
 # download CDC community levels
-cdc_comm_data = 'https://data.cdc.gov/api/views/3nnm-4jni/rows.csv?accessType=DOWNLOAD' %>% url() %>% readr::read_csv()
+cdc_comm_data = 'https://data.cdc.gov/api/views/3nnm-4jni/rows.csv?accessType=DOWNLOAD' %>% url() %>% read.csv(., header = TRUE)
 
 # filter to get Massachusetts data
-cdc_comm_ma = cdc_comm_data %>% dplyr::filter(state == 'Massachusetts' & date_updated == max(date_updated))
+cdc_comm_ma = cdc_comm_data %>% dplyr::mutate(date_updated = as.Date(date_updated, format = '%Y-%m-%d')) %>% dplyr::filter(state == 'Massachusetts' & date_updated == max(date_updated))
 
 # remove data where county is Martha's Vineyard or Nantucket
 cdc_comm_ma %<>% dplyr::filter(!county %in% c('Dukes County, MA', 'Nantucket County, MA')) %>% tidyr::separate(county, c('county', 'state_abb'), sep = ',')
@@ -21,11 +21,11 @@ cdc_tran_ma = cdc_tran_data %>% dplyr::mutate(report_date = as.Date(report_date,
 # # remove data where county is Martha's Vineyard or Nantucket
 cdc_tran_ma %<>% dplyr::filter(!county_name %in% c('Dukes County', 'Nantucket County'))
 
-# # combine county-level data
-# cdc_county = cdc_comm_ma %>%
-#   dplyr::select(state, state_abb, county, date_updated, `covid-19_community_level`) %>%
-#   rename(date_community = date_updated) %>%
-#   left_join(cdc_tran_ma %>% dplyr::select(state_name, county_name, report_date, community_transmission_level) %>% rename(state = state_name, county = county_name, date_transmission = report_date), by = c('state', 'county'))
+# combine county-level data
+cdc_county = cdc_comm_ma %>%
+  dplyr::select(state, state_abb, county, date_updated, covid.19_community_level) %>%
+  rename(date_community = date_updated) %>%
+  left_join(cdc_tran_ma %>% dplyr::select(state_name, county_name, report_date, community_transmission_level) %>% rename(state = state_name, county = county_name, date_transmission = report_date), by = c('state', 'county'))
 
 # save data
-write.csv(cdc_tran_ma, './data/cdc-ma-counties.csv', row.names = FALSE)
+write.csv(cdc_county, './data/cdc-ma-counties.csv', row.names = FALSE)
